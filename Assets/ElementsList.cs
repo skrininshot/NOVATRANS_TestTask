0fdc91a9b1a58f6c6c812bcd7fb8544f068290aa
Егор Скринин <skrininshot@mail.ru>
2023-08-29 00:59:10 +0500

    Load list elements from object childs

---
+++ Assets/ElementsList.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementsList : MonoBehaviour
+{
+    [SerializeField] private Transform _elementsParent;
+    [SerializeField] private ListElement _listElementPrefab;
+    private List<ListElement> _listElements = new();
+
+    private void Awake()
+    {
+        if (_elementsParent != null)
+        {
+            for (int i = 0; i < _elementsParent.childCount; i++)
+            {
+                ListElement newElement = Instantiate(_listElementPrefab, transform);
+                newElement.SetElement(_elementsParent.GetChild(i));
+                _listElements.Add(newElement);
+            }
+        } 
+    }
+}
---
+++ Assets/ListElement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+public class ListElement : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private Transform _element;
+
+    private void OnValidate()
+    {
+        _text = GetComponentInChildren<TMP_Text>();
+    }
+
+    public void SetElement(Transform element)
+    {
+        _element = element;
+        _text.text = _element.name;
+    }
+}
